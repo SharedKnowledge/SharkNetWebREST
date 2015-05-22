9d90a3ae4fe6faf36153feae4216b1065838fa6b
Felix Brix <felix.brix@onlinehome.de>
2015-05-22 13:52:46 +0200

    Updated .gitignore and refactored JSON helper

--- src/java/Profile.java
+++ src/java/Profile.java
@@ -1,15 +1,33 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import net.sharkfw.apps.sharknet.SharkNet;
+import net.sharkfw.apps.sharknet.SharkNetException;
+import net.sharkfw.apps.sharknet.SharkNetPeerProfile;
+import net.sharkfw.apps.sharknet.j2se_android.SharkNetEngine;
+import net.sharkfw.knowledgeBase.PeerSemanticTag;
+import net.sharkfw.knowledgeBase.SharkKBException;
+import net.sharkfw.system.SharkSecurityException;
+import org.json.JSONObject;
 
 import toolbox.*;
 
 @WebServlet(urlPatterns = {"/profile"})
 public class Profile extends HttpServlet {
+    
+    private final SharkNet sharkNet;
+    private final JSON jsonHelper;
+    
+    public Profile() throws SharkKBException, SharkNetException, SharkSecurityException {
+        sharkNet = SharkNetEngine.createSharkNet("/Users/felixbrix/Documents/Studium/6. Semester/Entwicklung sozialer Anwendungen/SharkNetInterface/SharkNetAPI/db");
+        jsonHelper = new JSON();
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -19,23 +37,37 @@ public class Profile extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * @throws net.sharkfw.knowledgeBase.SharkKBException
+     * @throws net.sharkfw.apps.sharknet.SharkNetException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SharkKBException, SharkNetException {
         response.setContentType("application/json;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             
-            // Basic infos
-            JSON profile = new JSON("name", "Test Name");
-            profile.add("age", 27);
-    
-            // Links
-            JSON links = new JSON("twitter", "http://www.twitter.com/testname");
-            links.add("website", "http://www.testname.de");
-            profile.add("links", links.getBody());
+            // Creates example profile
+            PeerSemanticTag peerST = sharkNet.createPeerSemanticTag(
+                "felix",
+                new String[] { "https://github.com/fbrix", "http://brx-online.de" },
+                new String[] { "test@email.de", "tcp:test.de:7070" }
+            );
+                        
+            SharkNetPeerProfile peerProfile = sharkNet.createPeerProfile(peerST);
             
-            out.println(profile.toJSONString());
+            // Export profile as JSON string
+            out.println(jsonHelper.convertToString(peerProfile));
         }
+    }
+    
+    /**
+     * TODO dummy test method
+     * @return peer profile
+     * @throws SharkKBException
+     */
+    private void postProfile(String name, String[] si, String[] addr) throws SharkKBException, SharkNetException, SharkSecurityException
+    {
+        PeerSemanticTag peerST = sharkNet.createPeerSemanticTag(name, si, addr);
+        sharkNet.createPeerProfile(peerST);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -50,7 +82,11 @@ public class Profile extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SharkKBException | SharkNetException ex) {
+            Logger.getLogger(Profile.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -64,7 +100,11 @@ public class Profile extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SharkKBException | SharkNetException ex) {
+            Logger.getLogger(Profile.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
--- src/java/toolbox/JSON.java
+++ src/java/toolbox/JSON.java
@@ -1,18 +1,49 @@
 package toolbox;
 
 import java.util.HashMap;
+import net.sharkfw.apps.sharknet.SharkNetPeerProfile;
+import net.sharkfw.knowledgeBase.SharkKBException;
 import org.json.*;
 
 public class JSON {
     private final HashMap<String, Object> body;
+    
+    public JSON() {
+        body = new HashMap<>();
+    }
     
     public JSON(String key, Object value) {
        body = new HashMap<>();
        body.put(key, value);
     }
     
-    public void add(String key, Object value) {
+    public JSONObject convertToJson(SharkNetPeerProfile peerProfile) throws SharkKBException {
+        body.clear();
+        body.put("name", peerProfile.getPeer().getName());
+        
+        return new JSONObject(body);
+    }
+    
+    public String convertToString(SharkNetPeerProfile peerProfile) throws SharkKBException {
+        return convertToJson(peerProfile).toString();
+    }
+    
+    public void add(String key, String value) {
         body.put(key, value);
+    }
+    
+    public void add(String key, int value) {
+        body.put(key, value);
+    }
+    
+    public void add(String key, JSONObject value) {
+        body.put(key, value);
+    }
+    
+    public void add(String key, String[] values) {
+        for (String string: values) {
+            body.put(key, string);
+        }
     }
     
     public String toJSONString() {
